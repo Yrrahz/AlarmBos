0876ffc1ab3d5eb7d09603b26bd4a17eae88793a
Yrrah <harry.dfh@gmail.com>
2017-12-05 00:14:21 +0100

    Notification implement

--- app/src/main/java/booken/yrrah/alarmbos/MainActivity.java
+++ app/src/main/java/booken/yrrah/alarmbos/MainActivity.java
@@ -1,8 +1,11 @@
 package booken.yrrah.alarmbos;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,10 +18,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void notifyMeButton(View view){
+
+         // A small notification. Nothing much
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,resultIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        int notificationID = 1;
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.firstnotificationimage) // Appears that this must exist.
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!")
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        manager.notify(notificationID, mBuilder.build());
+
+
+        /* // This is the Service!
         Toast.makeText(getApplicationContext(),"NotifyMe is clicked.", Toast.LENGTH_SHORT).show();
 
         Intent intent = new Intent(this, AlarmBosService.class);
         //intent.setAction("booken.yrrah.alarmbos.action.FOO");
-        startService(intent);
+        startService(intent);*/
     }
 }
