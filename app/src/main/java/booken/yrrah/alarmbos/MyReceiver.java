5c2259f9c517d4ba3fb61acec299ec0c42b72c9f
Yrrah <harry.dfh@gmail.com>
2017-12-06 19:51:17 +0100

    First implementation of BroadcastReceiver. Apparently this was how we "listen" to incoming sms. At this point we can sense incoming sms and prints out a toast. Nothing concrete is done after it has sensed an incoming messege.

---
+++ app/src/main/java/booken/yrrah/alarmbos/MyReceiver.java
@@ -0,0 +1,34 @@
+package booken.yrrah.alarmbos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        //Bundle extras = intent.getExtras();
+        int duration = Toast.LENGTH_LONG;
+        Log.d("testReceiver", "Test begins...");
+        if(intent.getAction().equals("android.intent.action.AIRPLANE_MODE")){
+            //String state = extras.getString(TelephonyManager.EXTRA_STATE);
+            //Log.w("MY_DEBUG_TAG", state);
+
+            Toast toast = Toast.makeText(context,
+                    "Airplane mode is on or off.", duration);
+            toast.show();
+            Log.d("testReceiver", "Airplane mode is on or off");
+        }else if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            Toast toast = Toast.makeText(context,
+                    "SMS received", duration);
+            toast.show();
+            Log.d("testReceiver", "SMS was received");
+
+
+        }
+    }
+}
